b68a0252a71962470b2acfb8dbefb92dc2f75ba2
Sandeep <elsenothing321@gmail.com>
2024-07-20 10:59:44 +0530

    completed the folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+      <main>
+        <Main />
+      </main>
+      <Footer />
+    </>
   );
 }
 
